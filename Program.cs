7934aae0dd9fe05fc6bc315cd7a353f9f90645da
Rafael Ignacio Pinto <ignacio.rafael@gmail.com>
2019-03-25 08:23:02 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Akka.Actor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Steeltoe.Extensions.Configuration;
+using Steeltoe.Extensions.Configuration.ConfigServer;
+
+namespace akka.cluster
+{
+    class Program
+    {
+        private static IConfiguration Config { get; set; }
+
+        static void BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true)
+                .AddConfigServer()
+                .AddEnvironmentVariables();
+
+            Config = builder.Build();
+        }
+        static void Main(string[] args)
+        {
+            BuildConfiguration();
+            var clusterName = Config["clusterName"] ?? "localCluster";
+            var hocon = Config["hocon"];
+
+            var config = Akka.Configuration.ConfigurationFactory.ParseString(hocon);
+
+            using (var system = ActorSystem.Create(clusterName, config))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
